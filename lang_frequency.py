15d4506edb280a217d4faeb65cc646a0fe56162f
DjapelZed <maks.kushnirov@gmail.com>
2016-12-19 18:03:18 +0300

    Скелет частотного анализа.

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,18 @@
+from collections import Counter
 
 
 def load_data(filepath):
-    pass
+    file_open = open(filepath,'r')
+    file_data = file_open.read()
+    data = file_data.split()
+    return data
 
 
 def get_most_frequent_words(text):
-    pass
+#TODO: Игнорировать символ "—".
+    cntr = Counter(text)
+    return cntr.most_common(10)
 
 
 if __name__ == '__main__':
-    pass
+    print(get_most_frequent_words(load_data(input('File path: '))))
