af6a1a2737090f58c3d9f59165f3d61c300c60ea
DjapelZed <maks.kushnirov@gmail.com>
2016-12-20 00:07:34 +0300

    Переименнованы переменные

--- lang_frequency.py
+++ lang_frequency.py
@@ -4,20 +4,21 @@ from collections import Counter
 def load_data(filepath):
     file_open = open(filepath,'r')
     file_data = file_open.read()
-    data = file_data.split()
-    return data
+    data_split = file_data.split()
+    return data_split
 
 
 def get_most_frequent_words(text):
-    cntr = Counter(text)
-    top_words = cntr.most_common(10)
-    return top_words
+    counter = Counter(text)
+    most_frequent_words = counter.most_common(10)
+    return most_frequent_words
 
 
-def print_most_frequent_words(top_words):
-    for word_number, word in enumerate(top_words):
+def print_most_frequent_words(words):
+    for word_number, word in enumerate(words):
         print('{0}. {1}: {2}'.format(word_number + 1, word[0], word[1]))
 
 
 if __name__ == '__main__':
-    print_most_frequent_words(get_most_frequent_words(load_data(input('File path: '))))
+    filepath = input('File path: ')
+    print_most_frequent_words(get_most_frequent_words(load_data(filepath)))
